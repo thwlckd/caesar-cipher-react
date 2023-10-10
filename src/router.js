6bcf7817a8d642775978e0fa53c4bcb8200fef61
thwlckd <thwlckd@naver.com>
2023-10-10 13:34:25 +0900

    Feat: lazy loading 적용

--- src/router.js
+++ src/router.js
@@ -1,17 +1,40 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
-import Home from "./components/Home";
-import Encryption from "./components/Encryption";
-import Decryption from "./components/Decryption";
+import { Suspense, lazy } from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App';
+import Progress from './components/Progress';
+const Home = lazy(() => import('./components/Home'));
+const Encryption = lazy(() => import('./components/Encryption'));
+const Decryption = lazy(() => import('./components/Decryption'));
 
 const router = createBrowserRouter([
   {
-    path: "/caesar-cipher-react",
+    path: '/caesar-cipher-react',
     element: <App />,
     children: [
-      { path: "", element: <Home /> },
-      { path: "encryption", element: <Encryption /> },
-      { path: "decryption", element: <Decryption /> },
+      {
+        path: '',
+        element: (
+          <Suspense fallback={<Progress />}>
+            <Home />
+          </Suspense>
+        ),
+      },
+      {
+        path: 'encryption',
+        element: (
+          <Suspense fallback={<Progress />}>
+            <Encryption />
+          </Suspense>
+        ),
+      },
+      {
+        path: 'decryption',
+        element: (
+          <Suspense fallback={<Progress />}>
+            <Decryption />
+          </Suspense>
+        ),
+      },
     ],
   },
 ]);
